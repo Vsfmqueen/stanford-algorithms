17282a988244ab190e32adb048ba93eb6864f31f
vera_sidarovich <vera_sidarovich@epam.com>
2014-05-20 23:24:09 +0300

    added sort method

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -1,7 +1,45 @@
+import java.util.Arrays;
+
 public class QuickSort {
 
+	private static Integer[] array = { 3, 8, 2, 5, 1, 4, 7, 6 };
+
 	public static void main(String... args) {
-		System.out.println("Hello");
+
+		divideArray(array, 0, array.length);
 	}
 
+	private static void divideArray(Integer[] array, int start, int end) {
+		int middle = start + (end - start) / 2;
+
+		if (end <= start) {
+			return;
+		}
+
+		
+		divideArray(array, start, middle);
+		divideArray(array, middle + 1, end);
+		
+		System.out.println("Start = " + start + " End = " + end);
+		
+		sort(array, array[start], start);
+	}
+
+	private static void sort(Integer[] array, int pivotElement, int pivotIndex) {
+		int i = pivotIndex++;
+		int j = pivotIndex++;
+
+		int a = 0;
+
+		while (j < array.length) {
+			if (array[j] < pivotElement) {
+				a = array[i];
+				array[i] = array[j];
+				array[j] = a;
+				i++;
+			}
+			j++;
+		}
+		System.out.println(Arrays.deepToString(array));
+	}
 }
