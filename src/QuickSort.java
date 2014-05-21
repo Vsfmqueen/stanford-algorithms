6f96db4cdf4b818b33f57762c5cf9bd8c17a3f5f
vera_sidarovich <vera_sidarovich@epam.com>
2014-05-21 15:27:42 +0300

    done sort with middle usage

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -15,26 +15,48 @@ public class QuickSort {
 
     private static void sortArray(Integer[] array, int start, int end) {
 
+        boolean middlePivot = true;
+        
         if (end <= start) {
             return;
         }
 
-        int middle = partition(array, start, end);
-
+      //  int middle = calculateMiddleFirstCase(array, start, end);
+      //  int middle = calculateMiddleSecondCase(array, start, end);
+        int middle = partition(array, start, end, true);
+      
         sortArray(array, start, middle - 1);
         sortArray(array, middle + 1, end);
     }
 
-    private static int partition(Integer[] array, int start, int end) {
+    private static int calculateMiddleFirstCase(Integer[] array, int start, int end) {
+        boolean middlePivot = false;
+        return partition(array, start, end, middlePivot);
+    }
+    
+    private static int calculateMiddleSecondCase(Integer[] array, int start, int end) {
+        boolean middlePivot = false;
+        exchange(array, start, end);
+        return partition(array, start, end, middlePivot);
+    }
+    
+    private static int partition(Integer[] array, int start, int end, boolean middlePivot) {
 
         int arrayLength = end - start;
         comparasionCount += arrayLength;
 
         int i = start + 1;
         int j = start + 1;
+        int pivotElement;
+        
+        if (middlePivot) {
+            pivotElement = detectPivotElement(array, start, end);
+        } else {
+            pivotElement = array[start];
+        }
 
         while (j <= end) {
-            if (array[j] < array[start]) {
+            if (array[j] < pivotElement) {
                 exchange(array, i, j);
                 i++;
             }
@@ -52,5 +74,31 @@ public class QuickSort {
         a = array[firstIndex];
         array[firstIndex] = array[secondIndex];
         array[secondIndex] = a;
+    }
+    
+    private static int detectPivotElement(Integer[] array, int start, int end) {
+        int middle = (end - start) / 2;
+
+        int startElement = array[start];
+        int endElement = array[end];
+        int middleElement = array[middle];
+
+        int pivotElement;
+
+        if (startElement < endElement) {
+            pivotElement = findSmallerElement(middleElement, startElement);
+        } else {
+            pivotElement = findSmallerElement(middleElement, endElement);
+        }
+
+        return pivotElement;
+    }
+    
+    private static int findSmallerElement(int firstElement, int endElement) {
+        if (firstElement > endElement) {
+            return firstElement;
+        } else {
+            return endElement;
+        }
     }
 }
