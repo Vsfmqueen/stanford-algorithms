6f6b4fba07ccd11cafb88c08de49331061741877
vera_sidarovich <vera_sidarovich@epam.com>
2014-05-21 13:10:39 +0300

    done quick sort

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -2,44 +2,47 @@ import java.util.Arrays;
 
 public class QuickSort {
 
-	private static Integer[] array = { 3, 8, 2, 5, 1, 4, 7, 6 };
+    private static Integer[] array = { 3, 8, 2, 5, 1, 4, 7, 6 };
 
-	public static void main(String... args) {
+    public static void main(String... args) {
 
-		divideArray(array, 0, array.length);
-	}
+        sortArray(array, 0, array.length - 1);
+    }
 
-	private static void divideArray(Integer[] array, int start, int end) {
-		
+    private static void sortArray(Integer[] array, int start, int end) {
+
         if (end <= start) {
             return;
         }
 
-        int middle = start + (end - start) / 2;
-        
-		divideArray(array, start, middle);
-        divideArray(array, middle, end);
-		
-		System.out.println("Start = " + start + " End = " + end);
-		
-		sort(array, array[start], start);
-	}
+        int middle = partition(array, start, end);
 
-	private static void sort(Integer[] array, int pivotElement, int pivotIndex) {
-		int i = pivotIndex++;
-		int j = pivotIndex++;
+        sortArray(array, start, middle - 1);
+        sortArray(array, middle + 1, end);
+    }
 
-		int a = 0;
+    private static int partition(Integer[] array, int start, int end) {
+        int i = start + 1;
+        int j = start + 1;
 
-		while (j < array.length) {
-			if (array[j] < pivotElement) {
-				a = array[i];
-				array[i] = array[j];
-				array[j] = a;
-				i++;
-			}
-			j++;
-		}
-		System.out.println(Arrays.deepToString(array));
-	}
+        while (j <= end) {
+            if (array[j] < array[start]) {
+                exchange(array, i, j);
+                i++;
+            }
+            j++;
+        }
+
+        exchange(array, start, i - 1);
+        System.out.println(Arrays.deepToString(array));
+        return i - 1;
+    }
+
+    private static void exchange(Integer[] array, int firstIndex, int secondIndex) {
+        int a = 0;
+
+        a = array[firstIndex];
+        array[firstIndex] = array[secondIndex];
+        array[secondIndex] = a;
+    }
 }
